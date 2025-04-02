d1fad08109fbea23a3bdc3b27c60dce6a6b46ef7
davidchongg <98029502+davidchongg@users.noreply.github.com>
2025-04-03 02:38:05 +0800

    Fix invalid tuition time parsed successfully

--- src/main/java/seedu/address/model/person/TuitionTime.java
+++ src/main/java/seedu/address/model/person/TuitionTime.java
@@ -12,7 +12,8 @@ public class TuitionTime {
 
     public static final String MESSAGE_CONSTRAINTS =
             "Tuition time must be in the format: 'DayOfWeek, HHMM-HHMM'.\n"
-                    + "Day must be Monday to Sunday (case-insensitive), and times must be in 24-hour format.\n"
+                    + "Day must be Monday to Sunday (case-insensitive), times must be in 24-hour format and "
+                    + "start time cannot be later than end time.\n"
                     + "Example: Monday, 1000-1200 or Mon, 1000-1200";
 
     /**
@@ -21,10 +22,8 @@ public class TuitionTime {
      * Example of a valid string: "Monday, 1400-1600".
      */
     public static final String VALIDATION_REGEX =
-            "^(?i)(monday|tuesday|wednesday|thursday|friday|saturday|sunday|mon|tue|wed|thu|fri|sat|sun),"
-                    + "\\s\\d{4}-\\d{4}$";
-
-
+            "^(?i)(monday|tuesday|wednesday|thursday|friday|saturday|sunday|mon|tue|wed|thu|fri|sat|sun),\\s"
+                    + "(?:([01]?[0-9]|2[0-3])[0-5][0-9])-(?:([01]?[0-9]|2[0-3])[0-5][0-9])$";
 
     public final String value;
 
@@ -43,7 +42,23 @@ public class TuitionTime {
      * Returns true if a given string is a valid tuition time.
      */
     public static boolean isValidTuitionTime(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return test.matches(VALIDATION_REGEX) && TuitionTime.isStartTimeEarlierThanEndTime(test);
+    }
+
+    /**
+     * Compares the start time and end time to ensure that the start time is not later than the end time.
+     *
+     * @param input The input string in the format "DayOfWeek, HHMM-HHMM", which includes start and end times.
+     * @return true if the start time is earlier than or equal to the end time; false otherwise.
+     */
+    public static boolean isStartTimeEarlierThanEndTime(String input) {
+        String[] parts = input.split(",\\s");
+        String[] times = parts[1].split("-");
+
+        int endTime = Integer.parseInt(times[1]);
+        int startTime = Integer.parseInt(times[0]);
+
+        return startTime <= endTime;
     }
 
     @Override
